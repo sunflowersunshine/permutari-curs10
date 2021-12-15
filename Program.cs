b36541f9165eb41332ffecf2ec586675d090811d
Monica-Mariana Stoleru <monicastoleru7@gmail.com>
2021-12-15 12:19:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace permutari_curs10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            Console.WriteLine("Introduceti n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            new  permutariSolver(n).Solve();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ permutariSolver.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace permutari_curs10
+{
+ 
+     public class permutariSolver
+    {
+            private int n;
+            int[] stack;
+
+            public permutariSolver(int n)
+            {
+                this.n = n;
+                stack = new int[n];
+                for (int i = 0; i < stack.Length; i++)
+                {
+                    stack[i] = -1;
+                }
+            }
+
+            void InitializeStackLevel(int level, int[] stack)
+            {
+                stack[level] = 0;
+            }
+
+            bool LevelHasSuccessor(int level, int[] stack)
+            {
+                if (stack[level] < n)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            void SetLevelValue(int level, int[] stack)
+            {
+                stack[level] = stack[level] + 1;
+            }
+
+            bool LevelIsValid(int level, int[] stack)
+            {
+                for (var i = 0; i < level; i++)
+                {
+                    if (stack[i] == stack[level])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            bool IsSolution(int level)
+            {
+                if (level == n - 1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            void PrintSolution()
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    Console.Write(stack[i] + " ");
+                }
+                Console.Write("\n");
+            }
+
+            public void Solve()
+            {
+                var level = 0;
+                var isValid = true;
+                var hasSuccesor = true;
+
+                InitializeStackLevel(level, stack);
+
+                while (level >= 0)
+                {
+                    do
+                    {
+                        hasSuccesor = LevelHasSuccessor(level, stack);
+                        if (hasSuccesor)
+                        {
+                            SetLevelValue(level, stack);
+                            isValid = LevelIsValid(level, stack);
+                        }
+                    } while (!(!hasSuccesor || (hasSuccesor && isValid)));
+
+                    if (hasSuccesor)
+                    {
+                        if (IsSolution(level))
+                        {
+                            PrintSolution();
+                        }
+                        else
+                        {
+                            level++;
+                            InitializeStackLevel(level, stack);
+                        }
+                    }
+                    else
+                    {
+                        level--;
+                    }
+                }
+            }
+        }
+    }
+
